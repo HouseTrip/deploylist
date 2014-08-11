edeb913507bde007aca225b3faeee56e986b2a23
Jesper Kjeldgaard <jkjeldgaard@housetrip.com>
2014-08-11 11:40:48 +0100

    Add basic auth for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate, if: -> { Rails.env.production? }
+
+  private
+
+  def authenticate
+    http_basic_authenticate_with name: ENV.fetch('USERNAME'), password: ENV.fetch('PASSWORD')
+  end
+
 end
