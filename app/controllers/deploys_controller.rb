d37f4a3cbe11c27a6acc1eddf9dc7b184b4e54a3
Andy Shipman <ashipman@housetrip.com>
2014-08-27 11:31:41 +0100

    Add ping/pong for NewRelic availability check

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -2,4 +2,8 @@ class DeploysController < ApplicationController
   def index
     @deploys = Deploy.production.map { |d| DeployPresenter.new(d) }
   end
+
+  def ping
+    render :text => 'pong'
+  end
 end
