55bce5549805ad8acd26e056af3a3b52b466355d
Matthew Hutchinson <matt@hiddenloop.com>
2016-01-12 16:48:04 +0000

    revert change to use vendor/ruby

--- setup.rb
+++ setup.rb
@@ -2,7 +2,7 @@
 
 setup = [
   'cp .env.example .env',
-  'bundle install --path=vendor/ -j 4',
+  'bundle install -j 4',
   'bundle exec rake db:setup',
   'echo "*** review .env and update with correct details ***"'
 ].join(" && ")
