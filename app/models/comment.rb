20485b318c4749abf79f618bedd70ebf4ea3e9d9
Giulio Sante Giraldi <giulio@Giulios-MacBook-Pro-2.local>
2016-01-13 15:00:20 +0000

    testing comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :deploy
   belongs_to :user
+  
+  validates_presence_of :deploy, :on => :create, :message => "can't be blank"
+  validates_presence_of :user, :on => :create, :message => "can't be blank"
+  # and has a comment (text)
 end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Comment do
+  
+  describe "structure" do 
+    let(:user) { User.find_or_create_by(email: "something@somewhere.com", name: "jack")}
+    let(:deploy) { create :deploy }
+    
+    it do 
+      expect(Comment.new(user: user, comment: "some comment").valid?).to be false
+    end
+    
+    it do 
+      expect(Comment.new(deploy: deploy, comment: "some comment").valid?).to be false
+    end
+  end
+  
+end
